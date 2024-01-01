e427428671eac9507d75e06d972858e066c92801
Prashant Bahuguna <54200145+cryptic-glitch@users.noreply.github.com>
2024-01-01 16:59:57 +0100

    chg: regex

--- src/raw/dataset.py
+++ src/raw/dataset.py
@@ -1,47 +1,45 @@
-# Loading the dataset and pre-processing the data
+# Loading and Pre-Processing the dataset
 
-import string
+import spacy
 import pandas as pd
-import nltk
-from nltk.tokenize import word_tokenize
-from nltk.corpus import stopwords
-from nltk.stem import WordNetLemmatizer
+import regex
 
-
-nltk.download('punkt')
-nltk.download('stopwords')
-nltk.download('wordnet')
-# Pre-Processing the data
+nlp = spacy.load('en_core_web_sm')
 
 def pre_process(text):
 
-    #  lower case
     text = text.lower()
 
-    #  Remove puntuation
-    text = ''.join([char for char in text if char not in string.punctuation])
+    # remove single characters
+    text = regex.sub(r'\b[A-Za-z]\b', '', text)
 
-    #  Tokenization
-    tokens = word_tokenize(text)
+    # remove whitespace
+    text = regex.sub(r'\s+', ' ', text)
 
-    # Stop words like -> and, the etc
-    stop_words = set(stopwords.words('english'))
-    tokens = ' ' .join([word for word in text.split() if word.lower() not in stop_words])
+    # remove stand-alone numbers
+    # text = regex.sub(r'\b\d+\b', '', text)
 
-    # Lemmatization or Stemming
-    lemmatizer = WordNetLemmatizer()
-    tokens = ' '.join([lemmatizer.lemmatize(word) for word in text.split()])
+    # remove repeated characters
+    text = regex.sub(r'\b(\w)\1+\b', r'\1\1', text)
 
-    #  Joining
-    processed_text = " ".join(tokens)
+    # remove words with 1 or 2 characters
+    text = regex.sub(r'\b(?:\w{1,2})\b', '', text)
+
+    doc = nlp(text)
+    tokens = [token.lemma_.lower() for token in doc if not token.is_punct and not token.is_stop]
+
+    # Join tokens back to text
+    processed_text = ' '.join(tokens)
 
     return processed_text
+
 
 abs_file_path = "/home/prashant/Downloads/llm-detect-ai-generated-text/train_essays.csv"
 df = pd.read_csv(abs_file_path)
 
 df['processed_text'] = df['text'].apply(pre_process)
+df.to_csv('pre_preprocessed_dataset.csv')
+print("done")
 
-# output_file = "/home/prashant/Downloads/llm-detect-ai-generated-text/pre-processed.csv"
-# df.to_csv(output_file, index=False)
-# print("Done")
+output_file = "/home/prashant/Downloads/llm-detect-ai-generated-text/pre-processed111.csv"
+df.to_csv(output_file, index=False)
