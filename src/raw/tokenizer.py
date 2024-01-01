fb9ef87d12456106bffbd9c6cbf03de264b94f60
Prashant Bahuguna <54200145+cryptic-glitch@users.noreply.github.com>
2024-01-01 17:01:56 +0100

    chg: regex

--- src/raw/tokenizer.py
+++ src/raw/tokenizer.py
@@ -2,5 +2,9 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 import pandas as pd
 from dataset import df
 
+df = pd.read_csv('pre_preprocessed_dataset.csv')
 vectorizer = TfidfVectorizer()
 X_train_tfidf = vectorizer.fit_transform(df['processed_text'])
+
+
+
