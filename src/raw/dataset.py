1dd33b2ba3cf74bc8e583170575a864deb177868
Prashant Bahuguna <54200145+cryptic-glitch@users.noreply.github.com>
2024-01-09 15:33:49 +0100

    upt : Data Exploration

--- src/raw/dataset.py
+++ src/raw/dataset.py
@@ -3,11 +3,13 @@
 import spacy
 import pandas as pd
 import regex
+from sklearn.feature_extraction.text import TfidfVectorizer
+import joblib
+from tqdm import tqdm
 
 nlp = spacy.load('en_core_web_sm')
 
 def pre_process(text):
-
     text = text.lower()
 
     # remove single characters
@@ -15,9 +17,6 @@ def pre_process(text):
 
     # remove whitespace
     text = regex.sub(r'\s+', ' ', text)
-
-    # remove stand-alone numbers
-    # text = regex.sub(r'\b\d+\b', '', text)
 
     # remove repeated characters
     text = regex.sub(r'\b(\w)\1+\b', r'\1\1', text)
@@ -37,9 +36,14 @@ def pre_process(text):
 abs_file_path = "/home/prashant/Downloads/llm-detect-ai-generated-text/train_essays.csv"
 df = pd.read_csv(abs_file_path)
 
-df['processed_text'] = df['text'].apply(pre_process)
+tqdm.pandas(desc="Pre-processing")
+df['processed_text'] = df['text'].progress_apply(pre_process)
 df.to_csv('pre_preprocessed_dataset.csv')
-print("done")
+print("Pre-processing done")
 
-output_file = "/home/prashant/Downloads/llm-detect-ai-generated-text/pre-processed111.csv"
-df.to_csv(output_file, index=False)
+# TF-IDF Vectorization
+vectorizer = TfidfVectorizer(max_features=5000)
+X_train_tfidf = vectorizer.fit_transform(tqdm(df['processed_text'], desc="TF-IDF Vectorization"))
+
+joblib.dump(X_train_tfidf, 'X_train_tfidf.pkl')
+print("TF-IDF Vectorization done")
